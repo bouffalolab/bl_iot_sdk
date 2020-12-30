e685aa22b4807b410f71f6d9eea6aa878307c497
Thomas Hebb <tommyhebb@gmail.com>
2020-12-30 15:07:31 -0800

    Remove pyinstaller-packed binary version of flash_build.py
    No scripts actually use this version: the one reference to it, which
    I've also removed in this commit, was commented out. Having the binary
    version is confusing because it indicates a proprietary component where
    there is none. It's also of limited use, since it only works on 64-bit
    Linux (and, at least on my system, doesn't even run properly there).
    
    I disassembled the bytecode of the compiled version and the source
    version and verified that they are identical, so we are losing nothing
    with this deletion.

--- make_scripts_riscv/project.mk
+++ make_scripts_riscv/project.mk
@@ -188,7 +188,6 @@ ifeq ("$(OS)","Windows_NT")
 else
 ifeq ("$(CONFIG_CHIP_NAME)", "BL602")
 	@cd $(BL60X_SDK_PATH)/image_conf;python3 -m pip install -r requirements.txt; python3 flash_build.py $(PROJECT_NAME) $(CONFIG_CHIP_NAME)
-#@cd $(BL60X_SDK_PATH)/image_conf; ./flash_build $(PROJECT_NAME) $(CONFIG_CHIP_NAME)
 endif
 endif
 	@echo "Building Finish. To flash build output."
