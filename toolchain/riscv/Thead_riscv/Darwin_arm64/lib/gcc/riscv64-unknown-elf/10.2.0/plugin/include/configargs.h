81d6e528f35d33160bbf5defa7263fa385229116
qwang <qwang@bouffalolab.com>
2023-08-17 10:43:46 +0800

    [Toolchain] Upgrade T-Head toolchain to V2.6.1 B-20220906. Only keep Linux AMD64 version
    Signed-off-by: qwang <qwang@bouffalolab.com>

--- toolchain/riscv/Thead_riscv/Darwin_arm64/lib/gcc/riscv64-unknown-elf/10.2.0/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "/Users/wk/work/toolchain/xuantie-gnu-toolchain/riscv-gcc/configure --target=riscv64-unknown-elf --prefix=/Users/wk/work/toolchain/xuantie-gnu-toolchain/riscv64 --disable-shared --disable-threads --enable-languages=c,c++ --with-system-zlib --enable-tls --with-newlib --with-sysroot=/Users/wk/work/toolchain/xuantie-gnu-toolchain/riscv64/riscv64-unknown-elf --with-native-system-header-dir=/include --disable-libmudflap --disable-libssp --disable-libquadmath --disable-libgomp --disable-nls --disable-tm-clone-registry --src=.././riscv-gcc --enable-multilib --with-abi=lp64d --with-arch=rv64imafdc 'CFLAGS_FOR_TARGET=-Os   -mcmodel=medlow' 'CXXFLAGS_FOR_TARGET=-Os   -mcmodel=medlow'";
-static const char thread_model[] = "single";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { "abi", "lp64d" }, { "arch", "rv64imafdc" } };
--- toolchain/riscv/Thead_riscv/Linux_aarch64/Readme.txt
+++
@@ -1,1 +0,0 @@
-Build on docker ubuntu 20.04 ARM64 with source code https://github.com/T-head-Semi/xuantie-gnu-toolchain/commit/00a23af2e14261a4165a57f5c022b525dd62b5ac on MackBook Pro M1 Pro CPU
